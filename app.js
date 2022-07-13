fe8dfc8cbc5220d7c0c7484b84398158ace70e13
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-07-13 18:31:11 -0400

    Renamed some variables

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express'),
-	path 	= require('path'),
-	fs   	= require('fs'),
-	uuid   	= require('node-uuid');
+    path 	= require('path'),
+    fs   	= require('fs'),
+    uuid   	= require('node-uuid');
 
 var app = express(),
-	baseDeDatos = fs.readFileSync('./datos.json').toString();
+    dataBase = fs.readFileSync('./db.json').toString();
 
-var datos = JSON.parse(baseDeDatos);
+var data = JSON.parse(dataBase);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,79 +15,79 @@ app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.methodOverride());
 
-// Pagina de Inicio: http:localhost:4000
-app.get('/', function (req,res){
-	res.sendfile(__dirname + '/index.html');
+// Home page: http:localhost:4000
+app.get('/', function (request, response){
+	response.sendfile(__dirname + '/index.html');
 });
 
 // API REST
 
-// Mostrar todos los libros
-app.get('/libros', function(req,res) {
-	res.send(datos);
+// Show all the books
+app.get('/books', function(request, response) {
+	response.send(data);
 });
 
-// Mostrar el detalle de un libro
-app.get('/libros/:id', function(req,res,next) {
-	var dato;
+// Show the book details
+app.get('/books/:id', function(request, response, next) {
+	var record;
 
-	for ( var i=0; i<datos.length; i++ ) {
-		var libro = datos[i];
+	for (var i=0; i < data.length; i++) {
+		var book = data[i];
 
-		if (libro.id === req.params.id) {
-			dato = libro;
+		if (book.id === request.params.id) {
+			record = book;
 		}
 	}
 
-	if (dato) {
-		res.send(dato);
+	if (record) {
+		response.send(record);
 	} else {
-		res.statusCode = 500;
-		return res.send('No se encuentra el id.');
+		response.statusCode = 500;
+		return response.send('Don\'t found the id.');
 	}
 
 });
 
-// POST: crear un nuevo libro.
-app.post('/libros', function (req, res){
-	req.body.id = uuid.v1(4);
+// POST: Create a new book.
+app.post('/books', function (request, response){
+	request.body.id = uuid.v1(4);
 
-	datos.push(req.body);
+	data.push(request.body);
 
-	res.send(200, {id: req.body.id});
+	response.send(200, {id: request.body.id});
 });
 
-// PUT: Actualizar un libro.
-app.put('/libros/:id', function (req, res){
-	var libro;
+// PUT: Update a book.
+app.put('/books/:id', function (request, response){
+	var book;
 
-	for (var i = datos.length - 1; i >= 0; i--) {
-		libro = datos[i];
+	for (var i = data.length - 1; i >= 0; i--) {
+		book = data[i];
 
-		if(libro.id === req.params.id){
-			datos[i] = req.body;
+		if(book.id === request.params.id){
+			data[i] = request.body;
 		}
 	}
 
-	res.send(200);
+	response.send(200);
 });
 
-// DELETE: Eliminar un libro.
-app.delete('/libros/:id', function (req,res) {
+// DELETE: Delete a book.
+app.delete('/books/:id', function (request, response) {
 
-	var elementoEliminar;
+	var deteleRecord;
 
-	for ( var i=0; i<datos.length; i++ ) {
-		var libro = datos[i];
+	for (var i=0; i < data.length; i++) {
+		var book = data[i];
 
-		if (libro.id === req.params.id) {
-			elementoEliminar = i;
+		if (book.id === request.params.id) {
+			deteleRecord = i;
 		}
 	}
 
-	datos.splice(elementoEliminar, 1);
+	data.splice(deteleRecord, 1);
 
-	res.send(200);
+	response.send(200);
 	
 });
 
